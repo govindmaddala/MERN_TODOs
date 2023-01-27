f44054b5aef49552f41461ba07c23a5f6c84d9e2
govindmaddala <govindmvn@gmail.com>
2023-01-27 08:49:43 +0530

    Routing is added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import decodeToken from 'jwt-decode';
 
 import Index from './components/Authentication'
+import Register from './components/Authentication/Register';
 
 import Home from './components/Tasks/Home'
 import axios from 'axios';
+import UserNotFound from './components/UserNotFound';
 
 const App = () => {
 
@@ -29,21 +31,20 @@ const App = () => {
             setIsLogged(false);
         }
     }, []);
-
     const logoutFunction = () => {
         setUser(false);
         setIsLogged(false);
         localStorage.removeItem("AUTH_TOKEN");
     }
-
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Index setUser={setUser} setIsLogged={setIsLogged} />} />
-                <Route path="/home" element={<Home isLogged={isLogged} user={user} setUser={setUser} setIsLogged={setIsLogged} logoutFunction={logoutFunction} />} />
-            </Routes>
-        </Router>
+        // <Router>
+        <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Index setUser={setUser} setIsLogged={setIsLogged} />} />
+            <Route path="/home" element={<Home isLogged={isLogged} user={user} setUser={setUser} setIsLogged={setIsLogged} logoutFunction={logoutFunction} />} />
+            <Route path="*" element={<UserNotFound />} />
+        </Routes>
+        // </Router>
     )
 }
-
 export default App
